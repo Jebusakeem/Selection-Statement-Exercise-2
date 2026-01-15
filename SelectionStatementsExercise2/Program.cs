8d6345cbfeaa7a7d0c9c7720c6212d03ccf92e0c
Briun Greene <harmonickarma789@gmail.com>
2026-01-15 18:45:31 -0500

    Switch statement interaction

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("what is your favorite subject?");
+            string favoriteSubject = Console.ReadLine();
+
+            switch (favoriteSubject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("Math is great because it is consistent");
+                    break;
+                case "science":
+                    Console.WriteLine("Science holds many interesting mysteries");
+                    break;
+                case "english":
+                    Console.WriteLine("English is the 3rd most spoken native language");
+                    break;
+                case "history":
+                    Console.WriteLine("History is the backbone of culture");
+                    break;
+                case "spanish":
+                    Console.WriteLine("Spanish is the 2rd most spoken native language");
+                    break;
+                default:
+                    Console.WriteLine($"I bet a lot of people like {favoriteSubject} ");
+                    break;
+            }
         }
     }
 }
